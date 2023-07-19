4740dc4df033e03367e52449014aa09209c6eaac
faisalahamedJ <37343326+faisalahamedJ@users.noreply.github.com>
2023-07-19 11:01:51 +0530

    Add files via upload

---
+++ D3-Testing-master/src/Cyto/DB.jsx
@@ -0,0 +1,497 @@
+// export const cyto1 = [
+//     // { // node nparent
+//     //   data: { id: 'nparent' }
+//     // },
+//     {
+//         "data": {
+//             "id": "node1"
+//         },
+//         // locked: true,
+//         grabbable: false,
+         
+//     },
+
+//     {
+//         "data": {
+//             "id": "a",
+//             "label": "a",
+//             "parent": "node1",
+//         },
+//         classes: ['node123'],
+//         "style": {
+//             'background-image': 'url(src/images/container.png)',
+//             'background-fit': 'contain',
+//             "background-image-containment": "over",
+//             "height": '25px',
+//             "width": '25px'
+//           }
+//     },{
+//         "data": {
+//             "id": "b",
+//             "label": "b",
+//             "parent": "node1",
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "c",
+//             "label": "c",
+//             "parent": "node1",
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "d",
+//             "label": "d",
+//             "parent": "node1",
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "b",
+//             // "id": "9b9c83d8-6a19-4466-9b6d-9a212adfbe82",
+//             label: 'Edge from Node1 to Node2'
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "c",
+//             "id": "e3f9ea66-4884-479f-9344-689d92bda9a1"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "d",
+//             "id": "c2783999-4bfd-4877-98d5-4bbf20fce039"
+//         }
+//     }
+// ]
+
+// export const cyto2 = [
+    
+//     {
+//         "data": {
+//             "id": "b",
+//             "label": "b"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "c",
+//             "label": "c"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "d",
+//             "label": "d"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "e",
+//             "label": "e"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "f",
+//             "label": "f"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "g",
+//             "label": "g"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "h",
+//             "label": "h"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "i",
+//             "label": "i"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "a",
+//             "label": "a"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "j",
+//             "label": "j"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "k",
+//             "label": "k"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "l",
+//             "label": "l"
+//         }
+//     },{
+//         "data": {
+//             "id": "m",
+//             "label": "m"
+//         }
+//     },{
+//         "data": {
+//             "id": "n",
+//             "label": "n"
+//         }
+//     },{
+//         "data": {
+//             "id": "o",
+//             "label": "o"
+//         }
+//     },{
+//         "data": {
+//             "id": "p",
+//             "label": "p"
+//         }
+//     },{
+//         "data": {
+//             "id": "q",
+//             "label": "q"
+//         }
+//     },{
+//         "data": {
+//             "id": "r",
+//             "label": "r"
+//         }
+//     },{
+//         "data": {
+//             "id": "s",
+//             "label": "s"
+//         }
+//     },{
+//         "data": {
+//             "id": "t",
+//             "label": "t"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "b",
+//             "id": "1"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "c",
+//             "id": "2"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "d",
+//             "id": "3"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "e",
+//             "id": "4"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "f",
+//             "id": "5"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "g",
+//             "id": "6"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "h",
+//             "id": "7"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "i",
+//             "id": "8"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "j",
+//             "id": "9"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "k",
+//             "id": "10"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "l",
+//             "id": "11"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "m",
+//             "id": "12"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "n",
+//             "id": "13"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "o",
+//             "id": "14"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "p",
+//             "id": "15"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "q",
+//             "id": "16"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "r",
+//             "id": "17"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "s",
+//             "id": "18"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "t",
+//             "id": "19"
+//         }
+//     }
+// ]
+
+// export const cyto3 = [
+//     {
+//         group: 'nodes',
+//         "data": {
+//             "id": "a",
+//             "label": "a"
+//         },
+//         style: {
+//           height : "20px",
+//           width : "20px",
+//           backgroundColor: "red"  
+//         },
+        
+//     },
+//     {
+//         "data": {
+//             "id": "b",
+//             "label": "b"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "c",
+//             "label": "c"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "d",
+//             "label": "d"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "e",
+//             "label": "e"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "f",
+//             "label": "f"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "g",
+//             "label": "g"
+//         }
+//     },
+//     {
+//         "data": {
+//             "id": "h",
+//             "label": "h"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "b",
+//             "id": "9b9c83d8-6a19-4466-9b6d-9a212adfbe82"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "c",
+//             "id": "e3f9ea66-4884-479f-9344-689d92bda9a1"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "d",
+//             "id": "c2783999-4bfd-4877-98d5-4bbf20fce039"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "e",
+//             "id": "10e27641-56df-49ef-8bb4-a6d543394114"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "h",
+//             "target": "f",
+//             "id": "23d42ef4-44f6-487d-be74-7730d148e1f6"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "g",
+//             "id": "00fb6306-f19f-4009-8afa-5a3292047c0f"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "a",
+//             "target": "h",
+//             "id": "33e083c1-855b-4fd3-9c4c-7accec8131bd"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "h",
+//             "target": "h",
+//             "id": "d5d9bb98-21b0-413e-9bc9-0c9472c84030"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "c",
+//             "target": "c",
+//             "id": "9f976bc4-88d6-49d5-9d93-09d737d84f24"
+//         }
+//     },
+//         {
+//         "data": {
+//             "source": "e",
+//             "target": "e",
+//             "id": "38953817-966a-4959-931c-83fc55b94587"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "e",
+//             "target": "f",
+//             "id": "08872424-e7d1-4e21-8df4-ad2f48553958"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "e",
+//             "target": "c",
+//             "id": "5fddafb8-c61b-4c1d-85c0-955f2a2b012c"
+//         }
+//     },
+//     {
+//         "data": {
+//             "source": "g",
+//             "target": "f",
+//             "id": "075a80bd-ccbf-4cc9-9461-faba6301b2be"
+//         }
+//     }
+// ]
+
+// export const getCytoCircle = (nodecount) => {
+//     const db = []
+//     for (let i = 1; i < nodecount; i++) {
+//         db.push(
+//             {
+//                 "data": {
+//                     "id": "n" +  i,
+//                     "label": "n" +  i
+//                 }
+//             },
+//         )
+//     }
+//     for (let i = 2; i < nodecount; i++) {
+//         db.push(
+//             {
+//                 "data": {
+//                     "source": "n1",
+//                     "target": "n" +  i,
+//                     "id": i
+                   
+//                 }
+//             },
+//         )
+//     }
+//     return db
+// }
+
---
+++ D3-Testing-master/src/Cyto/cytoscape-react.jsx
@@ -0,0 +1,9 @@
+// import { Graph, Node, Edge } from 'cytoscape-react';
+
+// function NoddyComponent({ id }) {
+//     return (
+//         <div className="noddy-component">
+//             {id}
+//         </div>
+//     );
+// }
